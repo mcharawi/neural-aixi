fb712f707d5fd6c4097e001ca476e0bff5aac990
Mahamad Charawi <m.charawi@gmail.com>
2016-12-05 21:46:04 +0000

    Modded files...

--- other/main.py
+++ other/main.py
@@ -1,58 +1,81 @@
+from __future__ import print_function
 import gym
 import search_tree as st
 import network
 import time
 import numpy as np
 
-env = gym.make('SpaceInvaders-ram-v0')
-network = network.Network([129, 200, 200, 200, 129], 256.0, 100.0, 0.1)
-st = st.SearchTree(network, env.action_space.n, 1)
+f = open('results.txt','w') 
+
+envs = ['CartPole-v0', 'Pong-ram-v0', 'Roulette-v0']
+trainings = [2000, 5000, 10000]
+etas = [.1, .2, .4, .8]
+search_times = [1, 10, 100, 1000]
+reward_scales = [1.0, 1.0, 1.0]
+observation_scales = [10, 256, 1]
+observation_sizes = [5, 129, 2]
+env_index = -1
+for temp_env in envs:
+  env_index = env_index + 1
+  for training in trainings:
+    for eta in etas:
+      for search_time in search_times:
+        env = gym.make(temp_env)
+        net = network.Network([observation_sizes[env_index], 200, 200, observation_sizes[env_index]], observation_scales[env_index], reward_scales[env_index], eta)
+        tree = st.SearchTree(net, env.action_space.n, search_time)
 
 
-#Setting up to learn
-observation = env.reset()
-action = env.action_space.sample()
-total_reward = 0
-total_baseline_reward = 0
-training = 5000
-
-#Training
-for counter in range(10000):
-
-  # Construct the x vector
-  x_arr = np.append(observation, action)
-  i = 0
-  x = np.zeros((129,1))
-  for item in x_arr:
-    x[i][0] = np.array(item)
-    i = i + 1
-
-  #Take the selected action
-  observation, reward, done, info = env.step(action)
-
-  # Construct the y vector
-  y_arr = np.append(observation/network.obs_scale, reward/network.rew_scale)
-  i = 0
-  y = np.zeros((129,1))
-  for item in y_arr:
-    y[i][0] = np.array(item)
-    i = i + 1
+        #Setting up to learn
+        observation = env.reset()
+        action = env.action_space.sample()
+        total_reward = 0
+        total_baseline_reward = 0
 
 
-  # Update the network
-  st.update_model([(x,y)])
+        #Training
+        for counter in range(training+1000):
 
-  if training < counter:
-    action = env.action_space.sample()
-    total_baseline_reward = total_baseline_reward + reward
-  else:
-    action = st.get_action(observation)
-    total_reward = total_reward + reward
+          # Construct the x vector
+          x_arr = np.append(observation, action)
+          i = 0
+          x = np.zeros((observation_sizes[env_index],1))
+          for item in x_arr:
+            x[i][0] = np.array(item)
+            i = i + 1
 
-  #reset the environment if necessary
-  if done:
-    observation = env.reset()
+          #Take the selected action
+          observation, reward, done, info = env.step(action)
+
+          # Construct the y vector
+          y_arr = np.append(observation/net.obs_scale, reward/net.rew_scale)
+          i = 0
+          y = np.zeros((observation_sizes[env_index],1))
+          for item in y_arr:
+            y[i][0] = np.array(item)
+            i = i + 1
 
 
-print("The total reward is %d" % total_reward)
-print("The total baseline reward is %d" % total_baseline_reward)
+          # Update the network
+          tree.update_model([(x,y)])
+
+          if training > counter:
+            action = env.action_space.sample()
+            total_baseline_reward = total_baseline_reward + reward
+          else:
+            action = tree.get_action(observation)
+            total_reward = total_reward + reward
+
+          #reset the environment if necessary
+          if done:
+            observation = env.reset()
+        
+        p_1 = total_reward/1000
+        p_2 = total_baseline_reward/training
+        f.write("The environment is %s\n" % temp_env)
+        f.write("The eta is %d\n" % eta)
+        f.write("The time is %d\n" % search_time)
+        f.write("The training is %d\n" % training)
+        f.write("The total reward is %d\n" % p_1)
+        f.write("The total baseline reward is %d\n" % p_2)
+
+f.close()
--- other/search_tree.py
+++ other/search_tree.py
@@ -20,53 +20,52 @@ current_milli_time = lambda: int(round(time.time() * 1000))
 
 class SearchTree(object):
 
-    def __init__(self, model, action_space_size, time):
-	""" The 'model' is the model of the environment that
-	the search tree uses to run simulations and choose
-	the best actions. The 'action space size' is the
-	number of actions that are possible in a 
-	given environment. The 'time' is the number of
-	milliseconds to run the simulation for."""
-	self.model = model
-	self.simulation_time = time
-	self.action_space_size = action_space_size
+  def __init__(self, model, action_space_size, time):
+    """ The 'model' is the model of the environment that
+    the search tree uses to run simulations and choose
+    the best actions. The 'action space size' is the
+  	number of actions that are possible in a 
+    given environment. The 'time' is the number of
+    milliseconds to run the simulation for."""
+    self.model = model
+    self.simulation_time = time
+    self.action_space_size = action_space_size
 	
+  def update_model(self, x):
+    """ Update the search tree's model of the env
+    with the given  data. 'x' is the concatentationof the previous observation, and the action taken
+    and 'y' is the concatenation of the new observation
+    and the reward."""
+    self.model.update(x)
 
-    def update_model(self, x):
-	""" Update the search tree's model of the env
-	with the given  data. 'x' is the concatentation
-	of the previous observation, and the action taken
-	and 'y' is the concatenation of the new observation
-	and the reward."""
-	self.model.update(x)
+  def run_simulation(self, action, observation):
+    """The method that runs a playout simulation for a given
+    beginning action."""
+    input_array = np.append(observation, action)
+    i = 0
+    x = np.zeros((self.model.sizes[0],1))
+    for item in input_array:
+      x[i][0] = np.array(item)
+      i = i + 1
 
-    def run_simulation(self, action, observation):
-	"""The method that runs a playout simulation for a given
-	beginning action."""
-	input_array = np.append(observation, action)
-	i = 0
-	x = np.zeros((129,1))
-	for item in input_array:
-		x[i][0] = np.array(item)
-		i = i + 1
+    out = self.model.feedforward(input_array)
+    reward = out[-1][0]
+    start_time = current_milli_time()
+    while current_milli_time() - start_time < self.simulation_time:
+      out = out * self.model.obs_scale
+      out[-1][0] = random.randint(0, self.action_space_size-1)
+      out = self.model.feedforward(out)
+      reward = reward + self.model.rew_scale*out[-1][0]
+    return reward
 
-	out = self.model.feedforward(input_array)
-	reward = out[-1][0]
-	start_time = current_milli_time()
-	while current_milli_time() - start_time < self.simulation_time:
-		out = out * 255
-		out[-1][0] = random.randint(0, self.action_space_size-1)
-		out = self.model.feedforward(out)
-		reward = reward + 100*out[-1][0]
-	return reward
 
-    def get_action(self, observation):
-	"""Get the best possible action given the current
-	model of the environment."""
-	rewards = []
-	for action in range(self.action_space_size):
-		rewards.append(self.run_simulation(action, observation))
-	return np.argmax(rewards)
+  def get_action(self, observation):
+    """Get the best possible action given the current
+    model of the environment."""
+    rewards = []
+    for action in range(self.action_space_size):
+      rewards.append(self.run_simulation(action, observation))
+    return np.argmax(rewards)
 
 
 
